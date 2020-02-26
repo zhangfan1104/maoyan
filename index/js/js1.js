81d16d537307e059b1685e9d276b762b5e0be47a
zf <869049026@qq.com>
2020-02-26 13:25:58 +0800

    添加首页

---
+++ index/js/js1.js
@@ -0,0 +1,13 @@
+$('.my').mouseover(function() {
+			$(this).find('.li1').show();
+		})
+		$('.my').mouseout(function() {
+			$(this).find('.li1').hide();
+		})
+		$('.app-download').mouseover(function() {
+			$(this).find('.li1').show();
+		})
+		$('.app-download').mouseout(function() {
+			$(this).find('.li1').hide();
+		})
+	
